23bf542ededbbee4ab804f51026b075f6ede6515
UlanZhassanov <ulanzhasanov@gmail.com>
2022-10-27 15:41:14 +0600

    css virtual managers

--- resources/views/front/virtual_admission.blade.php
+++ resources/views/front/virtual_admission.blade.php
@@ -3,51 +3,238 @@
     {{ __('Академия Гражданской Авиации') }}
 @endsection
 @section('content')
-<section id="services" class="services" style="background: #fff">
-    <div class="container" style="margin-bottom: 5vw">
-        <img src="/virtual consultations.jpeg" alt="">
-      <div class="row">
-        <div class="col-sm-6 col-lg-4 d-flex align-items-stretch mt-4 wow fadeInUp">
-            <p><img align="center" alt="" src="/assets/images/upload/antikor.png" style="width: 100%;" />&nbsp;</p>
-        </div>
-        <div class="col-sm-6 col-lg-8 align-items-stretch mt-4 wow fadeInUp">
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">Комплаенс-офицер</a></h4>
-            <p></p>
-          </div>
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">Цели и задачи </a></h4>
-            <p></p>
-          </div>
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">Нормативно правовые и иные акты в сфере противодействия коррупции</a></h4>
-            <p></p>
-          </div>
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">Внутренние НПА</a></h4>
-            <p></p>
-          </div>
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">Внутренний анализ коррупционных рисков</a></h4>
-            <p></p>
-          </div>
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">НПА по Международному стандарту ISO 37001:2016</a></h4>
-            <p></p>
-          </div>
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">Мероприятия в рамках профилактики противодействия коррупции</a></h4>
-            <p></p>
-          </div>
-          <div class="icon-box antikor col-12">
-            <h4><a class="stretched-link" href="#">Аудиоматериалы, направленные на продвижение антикоррупционной политики и формирование рациональной идеологии</a></h4>
-            <p></p>
-          </div>
-        </div>
+    <div id="accordion">
+        <div class="card">
+            <div class="card-header "><a class="card-link collapsed" data-toggle="collapse" href="#collapseOne">Менеджер по работе со студентами 1-го курса</a></div>
 
-      </div>
-
+            <div class="collapse show" data-parent="#accordion" id="collapseOne" style="height: 0px;overflow: hidden;">
+                <div class="card-body">
+                    <section class="services" id="services" style="background:#fff; padding:0">
+                        <div class="container">
+                            <div class="row">
+                                <div class="icon-box antikor col-12">
+                                    <h4><img align="center" alt=""
+                                        src="https://caa.edu.kz/assets/images/upload/email-western-libraries-112.png"
+                                        style="width: 30px; height: 30px;" /><a style="padding-left: 40px;" class="stretched-link" href="mailto:a.saparhan@agakaz.kz">Написать на электронную почту: a.saparhan@agakaz.kz</a></h4>
+                                </div>
+                                <div class="icon-box antikor col-12">
+                                    <h4><img align="center" alt=""
+                                        src="https://caa.edu.kz/assets/images/upload/WhatsApp_icon_1666431623.png"
+                                        style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://wa.me/+77473777719">Связаться по
+                                        WhatsApp +7 747 377 77 19</a></h4>
+                                </div>
+                                <div class="icon-box antikor col-12">
+                                    <h4><img align="center" alt=""
+                                        src="https://caa.edu.kz/assets/images/upload/800px-Telegram_Messenger_1666431682.png"
+                                        style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://t.me/+77473777719">Связаться по
+                                            Telegram</a></h4>
+                                </div>
+                                <div class="icon-box antikor col-12">
+                                    <h4><img align="center" alt=""
+                                        src="https://caa.edu.kz/assets/images/upload/whereby-vector-logo_1666431892.png"
+                                        style="width: 63px; height: 35px;" /><a style="padding-left: 7px;" class="stretched-link" href="https://whereby.com/manager-1.caa">Онлайн
+                                            консультация</a></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
     </div>
 
-  </section>
+    <div class="card">
+        <div class="card-header"><a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">Менеджер по работе со студентами 2-го курса</a></div>
+
+        <div class="collapse show" data-parent="#accordion" id="collapseTwo" style="height: 0px;overflow: hidden;">
+            <div class="card-body">
+
+                <section class="services" id="services" style="background:#fff; padding:0">
+                    <div class="container">
+                        <div class="row">
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/email-western-libraries-112.png"
+                                    style="width: 30px; height: 30px;" /><a style="padding-left: 40px;" class="stretched-link" href="mailto:d.idrisova@agakaz.kz">Написать на электронную почту: d.idrisova@agakaz.kz</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/WhatsApp_icon_1666431623.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://wa.me/+77077937813">Связаться по
+                                    WhatsApp +7 707 793 78 13</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/800px-Telegram_Messenger_1666431682.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://t.me/+77077937813">Связаться по
+                                        Telegram</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/whereby-vector-logo_1666431892.png"
+                                    style="width: 63px; height: 35px;" /><a style="padding-left: 7px;" class="stretched-link" href="https://whereby.com/manager-2.caa">Онлайн
+                                        консультация</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header"><a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">Менеджер по работе со студентами 3-го курса</a></div>
+
+        <div class="collapse show" data-parent="#accordion" id="collapseThree" style="height: 0px;overflow: hidden;">
+            <div class="card-body">
+
+                <section class="services" id="services" style="background:#fff; padding:0">
+                    <div class="container">
+                        <div class="row">
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/email-western-libraries-112.png"
+                                    style="width: 30px; height: 30px;" /><a style="padding-left: 40px;" class="stretched-link" href="mailto:e.kosherbaeva@agakaz.kz">Написать на электронную почту: e.kosherbaeva@agakaz.kz</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/WhatsApp_icon_1666431623.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://wa.me/+77477707721">Связаться по
+                                    WhatsApp +7 747 770 77 21</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/800px-Telegram_Messenger_1666431682.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://t.me/+77477707721">Связаться по
+                                        Telegram</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/whereby-vector-logo_1666431892.png"
+                                    style="width: 63px; height: 35px;" /><a style="padding-left: 7px;" class="stretched-link" href="https://whereby.com/manager-3.caa">Онлайн
+                                        консультация</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header"><a class="collapsed card-link" data-toggle="collapse" href="#collapseFour">Менеджер по работе со студентами 4-го курса</a></div>
+
+        <div class="collapse show" data-parent="#accordion" id="collapseFour" style="height: 0px;overflow: hidden;">
+            <div class="card-body">
+
+                <section class="services" id="services" style="background:#fff; padding:0">
+                    <div class="container">
+                        <div class="row">
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/email-western-libraries-112.png"
+                                    style="width: 30px; height: 30px;" /><a style="padding-left: 40px;" class="stretched-link" href="mailto:d.kalasheva@agakaz.kz">Написать на электронную почту: d.kalasheva@agakaz.kz</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/WhatsApp_icon_1666431623.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://wa.me/+77088341330">Связаться по
+                                    WhatsApp +7 708 834 13 30</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/800px-Telegram_Messenger_1666431682.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://t.me/+77088341330">Связаться по
+                                        Telegram</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/whereby-vector-logo_1666431892.png"
+                                    style="width: 63px; height: 35px;" /><a style="padding-left: 7px;" class="stretched-link" href="https://whereby.com/manager-4.caa">Онлайн
+                                        консультация</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header"><a class="collapsed card-link" data-toggle="collapse" href="#collapseFive">Председатель комитета по делам молодежи</a></div>
+
+        <div class="collapse show" data-parent="#accordion" id="collapseFive" style="height: 0px;overflow: hidden;">
+            <div class="card-body">
+
+                <section class="services" id="services" style="background:#fff; padding:0">
+                    <div class="container">
+                        <div class="row">
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/email-western-libraries-112.png"
+                                    style="width: 30px; height: 30px;" /><a style="padding-left: 40px;" class="stretched-link" href="mailto:kamilya.myrzakhanova@agakaz.kz">Написать на электронную почту: kamilya.myrzakhanova@agakaz.kz</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/WhatsApp_icon_1666431623.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://wa.me/+77471453711">Связаться по
+                                    WhatsApp +7 747 145 3711</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/800px-Telegram_Messenger_1666431682.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://t.me/+77471453711">Связаться по
+                                        Telegram</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/whereby-vector-logo_1666431892.png"
+                                    style="width: 63px; height: 35px;" /><a style="padding-left: 7px;" class="stretched-link" href="https://whereby.com/yc.caa">Онлайн
+                                        консультация</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-header"><a class="collapsed card-link" data-toggle="collapse" href="#collapseSix">Медицинское обслуживание</a></div>
+
+        <div class="collapse show" data-parent="#accordion" id="collapseSix" style="height: 0px;overflow: hidden;">
+            <div class="card-body" style="">
+
+                <section class="services" id="services" style="background:#fff; padding:0">
+                    <div class="container">
+                        <div class="row">
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/email-western-libraries-112.png"
+                                    style="width: 30px; height: 30px;" /><a style="padding-left: 40px;" class="stretched-link" href="mailto:b.lakova@agakaz.kz">Написать на электронную почту: b.lakova@agakaz.kz</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/WhatsApp_icon_1666431623.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://wa.me/+77083211454">Связаться по
+                                    WhatsApp +7 708 321 1454</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/800px-Telegram_Messenger_1666431682.png"
+                                    style="width: 35px; height: 35px;" /><a style="padding-left: 35px;" class="stretched-link" href="https://t.me/+77083211454">Связаться по
+                                        Telegram</a></h4>
+                            </div>
+                            <div class="icon-box antikor col-12">
+                                <h4><img align="center" alt=""
+                                    src="https://caa.edu.kz/assets/images/upload/whereby-vector-logo_1666431892.png"
+                                    style="width: 63px; height: 35px;" /><a style="padding-left: 7px;" class="stretched-link" href="https://whereby.com/medic.caa">Онлайн
+                                        консультация</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
 @endsection
