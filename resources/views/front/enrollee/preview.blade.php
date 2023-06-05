0b96a76e1d1ab89666cc3274a37ce91736b33ced
UlanZhassanov <ulanzhasanov@gmail.com>
2023-06-05 12:46:51 +0600

    translate enrollee preview

--- resources/views/front/enrollee/preview.blade.php
+++ resources/views/front/enrollee/preview.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <section id="enrollee">
         <div class="container">
-            <h1>Онлайн регистрация абитуриентов</h1>
+            <h1>{{ __('Онлайн регистрация абитуриентов') }}</h1>
             <div class="preview">
-                <a href="{{ route('front.enrollee.bachelor.index') }}" class="preview__item">Бакалавриат</a>
-                <a href="{{ route('front.enrollee.master.index') }}" class="preview__item">Магистратура</a>
-                <a href="{{ route('front.enrollee.doctoral.index') }}" class="preview__item">Докторантура</a>
+                <a href="{{ route('front.enrollee.bachelor.index') }}" class="preview__item">{{ __('Бакалавриат') }}</a>
+                <a href="{{ route('front.enrollee.master.index') }}" class="preview__item">{{ __('Магистратура') }}</a>
+                <a href="{{ route('front.enrollee.doctoral.index') }}" class="preview__item">{{ __('Докторантура') }}</a>
             </div>
         </div>
     </section>
