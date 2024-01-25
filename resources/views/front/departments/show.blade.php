b84084ab26f4553840ba6770a4c392dd5f25cc67
UlanZhassanov <ulanzhasanov@gmail.com>
2024-01-25 18:19:45 +0600

    delete op from eng department

--- resources/views/front/departments/show.blade.php
+++ resources/views/front/departments/show.blade.php
@@ -15,7 +15,7 @@
             <div class="departmentBanner">
                 {!! __($department->name) !!}
             </div>
-            @if (empty($eduP) == false)
+            @if (empty($eduP) == false && $department->name !== "Авиационный английский язык")
                 <hr class="col-12" style="height: 5px; background:#0e2465; max-width: -webkit-fill-available;" />
                 <h2 align="center">Образовательные программы</h2><br />
                 <div class="d-flex flex-wrap">
