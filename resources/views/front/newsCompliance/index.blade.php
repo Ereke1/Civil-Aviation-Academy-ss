43f5429b9b1a1c5128f22de0a23ec8ee43f2cb91
UlanZhassanov <ulanzhasanov@gmail.com>
2024-02-28 11:26:33 +0600

    newsCompliance fix

--- resources/views/front/newsCompliance/index.blade.php
+++ resources/views/front/newsCompliance/index.blade.php
@@ -46,32 +46,27 @@
             <div class="news">
                 @foreach ($data as $item)
                     <div class="news__preview">
-                        <img src="/storage/news/@if (Config::get('app.locale') === 'ru'){!! unserialize($item->bg_images)->ru !!}@elseif(Config::get('app.locale') === 'kk')@if(empty(unserialize($item->bg_images)->kk)){!! unserialize($item->bg_images)->ru !!}@else{!! unserialize($item->bg_images)->kk !!}@endif @elseif(Config::get('app.locale') === 'en')@if(empty(unserialize($item->bg_images)->en)){!! unserialize($item->bg_images)->ru !!}@else{!! unserialize($item->bg_images)->en !!}@endif @endif" />
-                        <div class="content">
+                        <a href="{{ route('front.news.show', $item->slug) }}">
+                            <img src="/storage/news/@if (Config::get('app.locale') === 'ru'){!! unserialize($item->bg_images)->ru !!}@elseif(Config::get('app.locale') === 'kk')@if(empty(unserialize($item->bg_images)->kk)){!! unserialize($item->bg_images)->ru !!}@else{!! unserialize($item->bg_images)->kk !!}@endif @elseif(Config::get('app.locale') === 'en')@if(empty(unserialize($item->bg_images)->en)){!! unserialize($item->bg_images)->ru !!}@else{!! unserialize($item->bg_images)->en !!}@endif @endif" />
+                        </a>
+                            <div class="content">
+                            <div class="content-title">
                             <h4>
-                                <a href="{{ route('front.newsCompliance.show', $item->slug) }}">
+                                <a href="{{ route('front.news.show', $item->slug) }}">
                                     @if (Config::get('app.locale') === 'ru')
-                                        {!! Str::limit(unserialize($item->titles)->ru, 40) !!}
+                                        {!! unserialize($item->titles)->ru !!}
                                     @elseif(Config::get('app.locale') === 'kk')
-                                        {!! Str::limit(unserialize($item->titles)->kk, 40) !!}
+                                        {!! unserialize($item->titles)->kk !!}
                                     @else
-                                        {!! Str::limit(unserialize($item->titles)->en, 40) !!}
+                                        {!! unserialize($item->titles)->en !!}
                                     @endif
                                 </a>
                             </h4>
-                            <p>
-                                @if (Config::get('app.locale') === 'ru')
-                                    {!! Str::limit(strip_tags(unserialize($item->descriptions)->ru), 160) !!}
-                                @elseif(Config::get('app.locale') === 'kk')
-                                    {!! Str::limit(strip_tags(unserialize($item->descriptions)->kk), 160) !!}
-                                @else
-                                    {!! Str::limit(strip_tags(unserialize($item->descriptions)->en), 160) !!}
-                                @endif
-                            </p>
-                            <div class="department">
+                            </div>
+                            {{-- <div class="department">
                                 <i class="fas fa-users"></i>
                                 <p>{!! $item->department !!}</p>
-                            </div>
+                            </div> --}}
                             <div class="date-time">
                                 <p><i class="far fa-calendar-alt"></i> {!! date('d.m.Y', strtotime($item->publish_at)) !!}</p>
                                 <p><i class="fas fa-clock"></i> {!! date('H:i', strtotime($item->publish_at)) !!}</p>
