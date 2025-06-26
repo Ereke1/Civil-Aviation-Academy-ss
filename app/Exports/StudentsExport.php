595028ed1dc030cf226ee0bf370437a4effbdbb7
AlisaAruФ <marionetkamir@mail.ru>
2025-06-26 16:14:16 +0500

    online reg export update

--- app/Exports/StudentsExport.php
+++ app/Exports/StudentsExport.php
@@ -1,14 +1,15 @@
 <?php
 
-
 namespace App\Exports;
 
 use Illuminate\Http\Request;
 use App\Models\RegistrationForTesting;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
 
-class StudentsExport implements FromQuery, WithHeadings
+class StudentsExport implements FromQuery, WithHeadings, WithMapping, WithColumnWidths
 {
     /** @var Request */
     protected $request;
@@ -24,7 +25,7 @@ class StudentsExport implements FromQuery, WithHeadings
 
         $query = RegistrationForTesting::query()
             ->where('is_confirmed', 1)
-            ->where('is_deleted',   0);
+            ->where('is_deleted', 0);
 
         if ($r->filled('have_ielts')) {
             $query->where('have_ielts', $r->have_ielts);
@@ -45,7 +46,7 @@ class StudentsExport implements FromQuery, WithHeadings
             $query->where('surname', 'like', '%'.$r->surname.'%');
         }
 
-        return $query->orderBy('updated_at','desc');
+        return $query->orderBy('updated_at', 'desc');
     }
 
     public function headings(): array
@@ -56,6 +57,44 @@ class StudentsExport implements FromQuery, WithHeadings
             'Дата интервью','Время интервью','Интервью пройдено','Дата регистрации',
         ];
     }
+
+    public function map($student): array
+    {
+        return [
+            $student->id,
+            $student->surname ?? '',
+            $student->name ?? '',
+            $student->patronymic ?? '',
+            $student->email ?? '',
+            $student->phone ?? '',
+            $student->have_ielts ? 'Да' : 'Нет',
+            $student->test_date ?? '',
+            $student->test_time_slot ?? '',
+            $student->test_passed ?? '',
+            $student->interview_date ?? '',
+            $student->interview_time_slot ?? '',
+            $student->interview_passed ?? '',
+            $student->created_at ? $student->created_at->format('Y-m-d H:i:s') : '',
+        ];
+    }
+
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 6,   // ID
+            'B' => 15,  // Фамилия
+            'C' => 15,  // Имя
+            'D' => 15,  // Отчество
+            'E' => 25,  // Email
+            'F' => 18,  // Телефон
+            'G' => 10,  // IELTS?
+            'H' => 14,  // Дата теста
+            'I' => 14,  // Время теста
+            'J' => 14,  // Тест пройден
+            'K' => 18,  // Дата интервью
+            'L' => 18,  // Время интервью
+            'M' => 14,  // Интервью пройдено
+            'N' => 20,  // Дата регистрации
+        ];
+    }
 }
-
-
